44a2583b44093bd5377387b41bcdeb2ae97c06b4
Mitch Smoot <mitchsmoot@gmail.com>
2018-08-31 16:11:53 -0600

    Updated default page title

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -5,7 +5,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portfolio Website"
+    @page_title = "Mitch's Portfolio Website"
     @seo_keywords = "Mitch Smoot Portfolio"
   end
 end
